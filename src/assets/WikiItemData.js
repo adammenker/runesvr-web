3170105dd436f1b2e6c35f9e183d60ca3c6578b9
Adam <adam.j.menker@gmail.com>
2022-12-14 16:23:13 -0600

    added wiki data assets

--- src/assets/WikiItemData.js
+++ src/assets/WikiItemData.js
@@ -1,24 +1,155 @@
+import axe from '../assets/axe.png';
+import boar from '../assets/boar.png';
+import bow from '../assets/bow.png';
+import crocodile from '../assets/crocodile.png';
+import dagger from '../assets/dagger.png';
+import hammer from '../assets/hammer.png';
+import rabbit from '../assets/rabbit.png';
+import shield from '../assets/shield.png';
+import snake_red from '../assets/snake_red.png';
+import snake_yellow from '../assets/snake_yellow.png';
+import spider_orange from '../assets/spider_orange.png';
+import spider_red from '../assets/spider_red.png';
 import sword from '../assets/sword.png';
-import mc_dog from '../assets/mc_dog.png';
+import wolf_black from '../assets/wolf_black.png';
+import wolf_grey from '../assets/wolf_grey.png';
 
-
-export const weapons = Array(5).fill({
-    id: 1,
-    name: 'Sword',
-    href: '#',
-    imageSrc: sword,
-    imageAlt: "Sword",
-    detail1: 'Damage: 14',
-    detail2: 'Durability: 25'
-})
+export const weapons = [
+    {
+        id: 1,
+        name: 'Axe',
+        href: '#',
+        imageSrc: axe,
+        imageAlt: "Axe",
+        detail1: 'Damage: 13',
+        detail2: 'Durability: 200'
+    },
+    {
+        id: 1,
+        name: 'Bow',
+        href: '#',
+        imageSrc: bow,
+        imageAlt: "Bow",
+        detail1: 'Damage: 9',
+        detail2: 'Durability: 50'
+    },
+    {
+        id: 1,
+        name: 'Dagger',
+        href: '#',
+        imageSrc: dagger,
+        imageAlt: "Dagger",
+        detail1: 'Damage: 18',
+        detail2: 'Durability: 75'
+    },
+    {
+        id: 1,
+        name: 'Hammer',
+        href: '#',
+        imageSrc: hammer,
+        imageAlt: "Hammer",
+        detail1: 'Damage: 11',
+        detail2: 'Durability: 250'
+    },
+    {
+        id: 1,
+        name: 'Shield',
+        href: '#',
+        imageSrc: shield,
+        imageAlt: "Shield",
+        detail1: 'Damage: 3',
+        detail2: 'Durability: 500'
+    },
+    {
+        id: 1,
+        name: 'Sword',
+        href: '#',
+        imageSrc: sword,
+        imageAlt: "Sword",
+        detail1: 'Damage: 25',
+        detail2: 'Durability: 125'
+    }
+]
 
 export const beasts = [
     {
         id: 1,
-        name: 'Dog',
+        name: 'Boar',
         href: '#',
-        imageSrc: mc_dog,
-        imageAlt: "Dog.",
+        imageSrc: boar,
+        imageAlt: "Boar",
+        detail1: 'Damage: 9',
+        detail2: 'Health: 32'
+    },
+    {
+        id: 1,
+        name: 'Crocodile',
+        href: '#',
+        imageSrc: crocodile,
+        imageAlt: "Crocodile",
+        detail1: 'Damage: 14',
+        detail2: 'Health: 26'
+    },
+    {
+        id: 1,
+        name: 'Rabbit',
+        href: '#',
+        imageSrc: rabbit,
+        imageAlt: "Rabbit.",
+        detail1: 'Damage: 0',
+        detail2: 'Health: 11'
+    },
+    {
+        id: 1,
+        name: 'Red Snake',
+        href: '#',
+        imageSrc: snake_red,
+        imageAlt: "Red Snake.",
+        detail1: 'Damage: 7',
+        detail2: 'Health: 14'
+    },
+    {
+        id: 1,
+        name: 'Yellow Snake',
+        href: '#',
+        imageSrc: snake_yellow,
+        imageAlt: "Yellow Snake.",
+        detail1: 'Damage: 6',
+        detail2: 'Health: 16'
+    },
+    {
+        id: 1,
+        name: 'Orange Spider',
+        href: '#',
+        imageSrc: spider_orange,
+        imageAlt: "Orange Spider.",
+        detail1: 'Damage: 8',
+        detail2: 'Health: 5'
+    },
+    {
+        id: 1,
+        name: 'Red Spider',
+        href: '#',
+        imageSrc: spider_red,
+        imageAlt: "Red Spider.",
+        detail1: 'Damage: 14',
+        detail2: 'Health: 28'
+    },
+    {
+        id: 1,
+        name: 'Black Wolf',
+        href: '#',
+        imageSrc: wolf_black,
+        imageAlt: "Black Wolf.",
+        detail1: 'Damage: 16',
+        detail2: 'Health: 56'
+    },
+    {
+        id: 1,
+        name: 'Grey Wolf',
+        href: '#',
+        imageSrc: wolf_grey,
+        imageAlt: "Grey Wolf.",
         detail1: 'Damage: 6',
         detail2: 'Health: 25'
     }
